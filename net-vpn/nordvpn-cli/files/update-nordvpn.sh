8f2278f8747f48e612a7706eb7472954fa20797d
Amedeo Salvati <amedeo@linux.com>
2020-05-02 18:49:37 +0200

    add update-nordvpn.sh for nordvpn-cli package

---
+++ net-vpn/nordvpn-cli/files/update-nordvpn.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+NORDVPN=/usr/sbin/nordvpn
+DEFAULT_OPT="best-udp"
+DEFAULT_COUNTRY="Italy"
+
+if [ ! -x $NORDVPN ]; then
+    echo "Nordvpn file $NORDVPN is not executable... Exit"
+    exit 1
+fi
+
+# first update the server list
+$NORDVPN update-servers
+
+if [ "$#" -ge 1 ]; then
+    COUNTRY=$@
+else
+    COUNTRY=$DEFAULT_COUNTRY
+fi
+
+$NORDVPN $DEFAULT_OPT "$COUNTRY"
+
